e7a54f395a1da8b070cd1fd2bf8d5f9d19ffaa94
lhy <you@example.com>
2023-08-29 21:43:49 +0800

    lhydev

--- front/src/utils/common.ts
+++ front/src/utils/common.ts
@@ -15,6 +15,45 @@ export function notice(
   });
 }
 
+export const uuid = (len?: number, radix?: number) => {
+  //生成一段随机的uuid唯一标识符
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz".split("");
+  const uuid = [];
+  let i;
+  radix = radix || chars.length;
+  if (len) {
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)];
+  } else {
+    let r;
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
+    uuid[14] = "4";
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | (Math.random() * 16);
+        uuid[i] = chars[i == 19 ? (r & 0x3) | 0x8 : r];
+      }
+    }
+  }
+  return uuid.join("");
+};
+
+export function getFileSize(fileByte: number) {
+  let fileSizeByte = fileByte;
+  let fileSizeMsg = "";
+  if (fileSizeByte < 1048576)
+    fileSizeMsg = (fileSizeByte / 1024).toFixed(2) + " KB";
+  else if (fileSizeByte == 1048576) fileSizeMsg = "1 MB";
+  else if (fileSizeByte > 1048576 && fileSizeByte < 1073741824)
+    fileSizeMsg = (fileSizeByte / (1024 * 1024)).toFixed(2) + " MB";
+  else if (fileSizeByte > 1048576 && fileSizeByte == 1073741824)
+    fileSizeMsg = "1 GB";
+  else if (fileSizeByte > 1073741824 && fileSizeByte < 1099511627776)
+    fileSizeMsg = (fileSizeByte / (1024 * 1024 * 1024)).toFixed(2) + " GB";
+  else fileSizeMsg = "文件超过1 TB";
+  return fileSizeMsg;
+}
+
+
 export function getToken(): string | null {
   return localStorage.getItem("zymtoken");
 }
